e48ee3007f9dedf84114d2a34f069b736fddbf2e
Elena Lozovskaya <elena.lozovskaya@gmail.com>
2017-12-12 13:33:32 +0200

    change mysite/settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^&0#v06ue^0*1*^tr6qp8kxw#dzq^fe6j2$2_ybouy3(hs3rv('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '<Asahy>.pythonanywhere.com']
 
